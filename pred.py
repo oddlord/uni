1ad503835cbad5d47da63755922bb941b4c4107d
tommy39 <tommy39@gmail.com>
2015-10-14 19:36:49 +0200

    Task contextmanager + per store average

--- pred.py
+++ pred.py
@@ -1,25 +1,34 @@
 from config import *
-from utils.utils import start_clock, end_clock
+from utils.utils import task
 
 import gzip
 from numpy import mean, ceil
 import shutil
 
-clock = start_clock('Training')
-with open(train, 'r') as f_in:
-    header = f_in.readline()
-    mean = ceil(mean([float(line.split(',')[3]) for line in f_in]))
-end_clock(clock)
+with task('Training'):
+    with open(train, 'r') as f_in:
+        f_in.readline()
+        stores = {}
+        for line in f_in:
+            store_id = int(line.split(',')[0])
+            if not store_id in stores:
+                stores[store_id] = {
+                    'sales': 0.0,
+                    'n': 0
+                }
+            stores[store_id]['sales'] += float(line.split(',')[3])
+            stores[store_id]['n'] += 1
+        for store_id  in stores:
+            stores[store_id]['mean'] = stores[store_id]['sales']/stores[store_id]['n']
 
-clock = start_clock('Prediction')
-with open(test, 'r') as f_in, open(predictions, 'w+') as f_out:
-    header = f_in.readline()
-    f_out.write('"Id","Sales"\n')
-    for line in f_in:
-        f_out.write("%s,%d\n" % (line.split(',')[0], mean))
-end_clock(clock)
+with task('Prediction'):
+    with open(test, 'r') as f_in, open(predictions, 'w+') as f_out:
+        f_in.readline()
+        f_out.write('"Id","Sales"\n')
+        for line in f_in:
+            store_id = int(line.split(',')[1])
+            f_out.write("%s,%.5f\n" % (line.split(',')[0], stores[store_id]['mean']))
 
-clock = start_clock('GZip compression')
-with open(predictions, 'r') as f_in, gzip.open(predictions_gz, 'w') as f_out:
-    shutil.copyfileobj(f_in, f_out)
-end_clock(clock)
+with task('GZip compression'):
+    with open(predictions, 'r') as f_in, gzip.open(predictions_gz, 'w') as f_out:
+        shutil.copyfileobj(f_in, f_out)
--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,10 @@
+from contextlib import contextmanager
 import sys
 import time
 
-def start_clock(task_name):
+@contextmanager
+def task(task_name):
     sys.stdout.write("%s started..." % (task_name))
-    return time.clock()
-
-def end_clock(start_time):
+    start_time = time.clock()
+    yield
     sys.stdout.write(" [Done] (%.4f s.)\n" % (time.clock() - start_time))
